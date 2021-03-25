567b3b2596ca6b4f72be763bb424320f2b8bcf04
Tom <tom@tombee.io>
2021-03-25 15:54:32 +0100

    Implemented basic dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  user: User | null = null;
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    // redirect to login page if no user
+    this.user = await this.authService.getUser();
+    if (!this.user) {
+      this.router.navigate(['login']);
+      return;
+    }
   }
-
 }
