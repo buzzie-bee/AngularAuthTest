14cc828c836bf0f060aa17157954e51554e22363
Tom <tom@tombee.io>
2021-03-25 14:15:06 +0100

    Implemented login component and basic tests

--- src/app/components/log-in/log-in.component.spec.ts
+++ src/app/components/log-in/log-in.component.spec.ts
@@ -1,25 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from '../../app-routing.module';
 
 import { LogInComponent } from './log-in.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LogInComponent', () => {
   let component: LogInComponent;
   let fixture: ComponentFixture<LogInComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LogInComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule({
+      declarations: [LogInComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LogInComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
     fixture.detectChanges();
+    router.initialNavigation();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form invalid when empty', () => {
+    expect(component.loginForm.controls.email.valid).toBeFalsy();
+  });
+
+  it('throws an error with an invalid email & password', () => {
+    component.loginForm.setValue({ email: 'test', password: '' });
+    expect(component.loginForm.controls.email.valid).toBeFalsy();
+    expect(component.loginForm.controls.password.valid).toBeFalsy();
+  });
+
+  it('is valid with a valid email & password', () => {
+    component.loginForm.setValue({ email: 'test@test.com', password: '1234' });
+    expect(component.loginForm.controls.email.valid).toBeTruthy();
+    expect(component.loginForm.controls.password.valid).toBeTruthy();
+  });
+
+  // TODO: Figure out how to check element with classname is in document
+  // it('show loading spinner when loading', () => {
+  //   component.loading = false;
+  //   const loadingButton = document.getElementsByClassName(
+  //     'submit-button-loading'
+  //   );
+  //   expect(loadingButton).toBeDefined();
+  // });
 });
--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.css'],
 })
 export class LogInComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  loginForm = this.fb.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,10}$'),
+      ],
+    ],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  loading: boolean = false;
+  hide: boolean = true;
+  failed: boolean = false;
+
+  ngOnInit(): void {}
+
+  async onSubmit(): Promise<void> {
+    const { email, password } = this.loginForm.value;
+    this.loading = true;
+    this.failed = false;
+
+    if (
+      this.loginForm.controls.email.errors ||
+      this.loginForm.controls.password.errors
+    ) {
+      this.loginForm.markAllAsTouched();
+      this.loading = false;
+      return;
+    }
+    const response = await this.authService.signIn({ email, password });
+
+    if (response === 'success') {
+      this.router.navigate(['/dashboard']);
+    }
+
+    this.failed = true;
+    this.loading = false;
   }
-
 }
